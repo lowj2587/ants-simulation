19a3c47beffda52a34c03e1a7807f6fb6c82923b
lowj2587 <102533264+lowj2587@users.noreply.github.com>
2024-09-18 10:30:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const { exec } = require('child_process');
+
+exec(`HOST=my-ants-simulation.onrender.com react-scripts start`, (error, stdout, stderr) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(stdout);
+  }
+});
 
